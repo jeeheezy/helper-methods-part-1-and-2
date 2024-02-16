9ff8fd591c373ef9dc50b159ce9e70aab157c7aa
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2024-02-16 20:45:32 +0000

    Refactored render statements

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,6 @@ class MoviesController < ApplicationController
   def new
     @the_movie = Movie.new
 
-    render template: "movies/new"
   end
 
   def index
@@ -15,9 +14,7 @@ class MoviesController < ApplicationController
         render json: @list_of_movies
       end
 
-      format.html do
-        render template: "movies/index"
-      end
+      format.html
     end
   end
 
@@ -27,8 +24,6 @@ class MoviesController < ApplicationController
     matching_movies = Movie.where({ :id => the_id })
 
     @the_movie = matching_movies.first
-
-    render templateL: "movies/show"
   end
 
   def create
@@ -40,7 +35,7 @@ class MoviesController < ApplicationController
       @the_movie.save
       redirect_to movies_path, notice:"Movie created successfully."
     else
-      render template: "movies/new"
+      render "new"
     end
   end
 
@@ -50,8 +45,6 @@ class MoviesController < ApplicationController
     matching_movies = Movie.where({ :id => the_id })
 
     @the_movie = matching_movies.first
-
-    render template: "movies/edit"
   end
 
   def update
