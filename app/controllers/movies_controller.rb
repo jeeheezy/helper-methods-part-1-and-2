49762f7126263fb28bc53eedf4a11c7a09acb4eb
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2024-02-17 00:31:57 +0000

    New movie form now nests movie attributes in subhash in params

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -24,8 +24,8 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new
-    @movie.title = params.fetch(:title)
-    @movie.description = params.fetch(:description)
+    @movie.title = params.fetch(:movie).fetch(:title)
+    @movie.description = params.fetch(:movie).fetch(:description)
 
     if @movie.valid?
       @movie.save
@@ -42,8 +42,8 @@ class MoviesController < ApplicationController
   def update
     movie = Movie.find(params[:id])
 
-    movie.title = params.fetch(:title)
-    movie.description = params.fetch(:description)
+    movie.title = params.fetch(:movie).fetch(:title)
+    movie.description = params.fetch(:movie).fetch(:description)
 
     if movie.valid?
       movie.save
